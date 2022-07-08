ac00b5579de5923d3f23b59825c0746a2c600a11
Marccudi <marccudi@hotmail.es>
2022-07-08 10:49:07 +0200

    Corrección ya que pedía que los metodos devolvieran un numero real

--- TA6Ej01/src/mainApp.java
+++ TA6Ej01/src/mainApp.java
@@ -6,15 +6,25 @@ public class mainApp {
 		// TODO Auto-generated method stub
 		String eleccion=JOptionPane.showInputDialog("1.Circulo 2.Triangulo 3.Cuadrado");
 		int figura = Integer.parseInt(eleccion);
+//		ya que el enunciado nos pide que los metodos nos devuelvan numero reales las preguntas las hacemos en el main en vez
+//		de en los metodos
 		switch (figura) {
 		case 1:
-			areaCirculo();
+			String pregunta = JOptionPane.showInputDialog("Escribe el radio del circulo:");
+			double radio = Double.parseDouble(pregunta);
+			System.out.println("El area del circulo con radio " + radio + " es de: " + areaCirculo(radio));
 			break;
 		case 2:
-			areaTriangulo();
+			String preguntaAlt = JOptionPane.showInputDialog("Escribe la altura del triangulo:");
+			double altura = Double.parseDouble(preguntaAlt);
+			String preguntaBas = JOptionPane.showInputDialog("Escribe la base del triangulo:");
+			double base = Double.parseDouble(preguntaBas);
+			System.out.println("El area del triangulo con altura" + altura + " y base" + base + " es de: " + areaTriangulo(base, altura));
 			break;
 		case 3:
-			areaCuadrado();
+			String preguntaLad = JOptionPane.showInputDialog("Introduce un lado del cuadrado: ");
+			double lado = Double.parseDouble(preguntaLad);
+			System.out.println("El area del cuadrado con lado " + lado + " es de: "+ areaCuadrado(lado));
 			break;
 		default:
 			System.out.println("Has introducido un valor equivocado");
@@ -22,27 +32,19 @@ public class mainApp {
 		}
 	}
 	
-	public static void areaCirculo() {
-		String pregunta = JOptionPane.showInputDialog("Escribe el radio del circulo:");
-		double radio = Double.parseDouble(pregunta);
+	public static double areaCirculo(double radio) {
 		double result = (Math.pow(radio, 2))* Math.PI;
-		System.out.println("El area del circulo con radio " + radio + " es de: " + result);
+		return result;
 	}
 
-	public static void areaTriangulo() {
-		String preguntaAlt = JOptionPane.showInputDialog("Escribe la altura del triangulo:");
-		double altura = Double.parseDouble(preguntaAlt);
-		String preguntaBas = JOptionPane.showInputDialog("Escribe la base del triangulo:");
-		double base = Double.parseDouble(preguntaBas);
+	public static double areaTriangulo(double base, double altura) {
 		double result = (base * altura) / 2;
-		System.out.println("El area del triangulo con altura" + altura + " y base" + base + " es de: " + result);
+		return result;
 	}
 	
-	public static void areaCuadrado() {
-		String pregunta = JOptionPane.showInputDialog("Introduce un lado del cuadrado: ");
-		double lado = Double.parseDouble(pregunta);
+	public static double areaCuadrado( double lado) {
 		double result = lado * lado;
-		System.out.println("El area del cuadrado con lado " + lado + " es de: "+ result);
+		return result;
 	}
 
 }
